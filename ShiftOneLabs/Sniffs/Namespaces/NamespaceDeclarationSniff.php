ad3eb689ece2e9458738b5965f629da5b3a74c70
Patrick Carlo-Hickman <patrick.carlohickman@gmail.com>
2016-11-03 19:40:19 +0000

    Initial commit.

---
+++ ShiftOneLabs/Sniffs/Namespaces/NamespaceDeclarationSniff.php
@@ -0,0 +1,101 @@
+<?php
+
+/**
+ * ShiftOneLabs_Sniffs_Namespaces_NamespaceDeclarationSniff.
+ *
+ * Ensures namespaces are declared correctly.
+ */
+class ShiftOneLabs_Sniffs_Namespaces_NamespaceDeclarationSniff implements PHP_CodeSniffer_Sniff
+{
+
+    /**
+     * Returns an array of tokens this test wants to listen for.
+     *
+     * @return array
+     */
+    public function register()
+    {
+        return array(T_NAMESPACE);
+    }
+
+    /**
+     * Processes this test, when one of its tokens is encountered.
+     *
+     * @param PHP_CodeSniffer_File  $phpcsFile  The file being scanned.
+     * @param int  $stackPtr  The position of the current token in the stack passed in $tokens.
+     *
+     * @return void
+     */
+    public function process(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
+    {
+        $tokens = $phpcsFile->getTokens();
+        $stackPrev = $stackPtr - 1;
+        $stackNext = $stackPtr + 1;
+
+        // One space after the namespace keyword.
+        if ($tokens[$stackNext]['content'] !== ' ') {
+            $error = 'There must be a single space after the NAMESPACE keyword';
+            $fix = $phpcsFile->addFixableError($error, $stackPtr, 'SpaceAfterNamespace');
+            if ($fix === true) {
+                $phpcsFile->fixer->replaceToken($stackNext, ' ');
+            }
+        }
+
+        // Find the previous token that is NOT whitespace
+        $lastNonWhitespace = $phpcsFile->findPrevious(T_WHITESPACE, $stackPrev, null, true);
+
+        // should not happen; something is horribly wrong
+        if ($lastNonWhitespace === false) {
+            return;
+        }
+
+        $diff = ($tokens[$stackPtr]['line'] - $tokens[$lastNonWhitespace]['line']);
+
+        if ($diff !== 2) {
+            $error = 'There must be one blank line before the namespace declaration';
+            $fix = $phpcsFile->addFixableError($error, $stackPtr, 'BlankLineBefore');
+
+            if ($fix === true) {
+                /*
+                 * If there is less than a 2 line difference, add newlines in.
+                 * If there is more than a 2 line difference, take newlines out.
+                 */
+                if ($diff < 2) {
+                    $phpcsFile->fixer->beginChangeset();
+                    for ($i = 0; $i < (2 - $diff); $i++) {
+                        $phpcsFile->fixer->addNewlineBefore($stackPtr);
+                    }
+                    $phpcsFile->fixer->endChangeset();
+                } else {
+                    /*
+                     * Remove all whitespace between the namespace and the previous non-whitespace
+                     * tokens. If a newline was removed from the same line as the previous
+                     * non-whitespace token, we need to make sure to add that back in.
+                     */
+                    $phpcsFile->fixer->beginChangeset();
+                    $newlineRemoved = false;
+                    for ($i = ($lastNonWhitespace + 1); $i < $stackPtr; $i++) {
+                        if ($tokens[$lastNonWhitespace]['line'] === $tokens[$i]['line']) {
+                            $newlineRemoved = true;
+                        }
+                        $phpcsFile->fixer->replaceToken($i, '');
+                    }
+                    if ($newlineRemoved) {
+                        $phpcsFile->fixer->addNewline($lastNonWhitespace);
+                    }
+                    $phpcsFile->fixer->addNewline($lastNonWhitespace);
+                    $phpcsFile->fixer->endChangeset();
+                }
+            }
+        } elseif ($tokens[$stackPtr]['column'] !== 1) {
+            $error = 'Line indented incorrectly; expected 0 spaces, found '.($tokens[$stackPtr]['column'] - 1);
+            $fix = $phpcsFile->addFixableError($error, $stackPtr, 'NoSpaceBeforeNamespace');
+
+            if ($fix === true) {
+                $content = explode("\n", $tokens[$stackPrev]['content']);
+                $content[count($content) - 1] = '';
+                $phpcsFile->fixer->replaceToken($stackPrev, implode("\n", $content));
+            }
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,31 @@
+# codesniffer-standard
+
+[![Latest Version on Packagist][ico-version]][link-packagist]
+[![Software License][ico-license]](LICENSE.txt)
+[![Total Downloads][ico-downloads]][link-downloads]
+
+This package adds a `ShiftOneLabs` standard to the [PHP_CodeSniffer](https://github.com/squizlabs/PHP_CodeSniffer) package. This is primarily useful for our internal development, but others are welcome to use it, as well.
+
+## Install
+
+Via Composer
+
+```bash
+$ composer require shiftonelabs/codesniffer-standard
+```
+
+## Security
+
+If you discover any security related issues, please email patrick@shiftonelabs.com instead of using the issue tracker.
+
+## License
+
+The MIT License (MIT). Please see [License File](LICENSE.txt) for more information.
+
+[ico-version]: https://img.shields.io/packagist/v/shiftonelabs/codesniffer-standard.svg?style=flat-square
+[ico-license]: https://img.shields.io/badge/license-MIT-brightgreen.svg?style=flat-square
+[ico-downloads]: https://img.shields.io/packagist/dt/shiftonelabs/codesniffer-standard.svg?style=flat-square
+
+[link-packagist]: https://packagist.org/packages/shiftonelabs/codesniffer-standard
+[link-downloads]: https://packagist.org/packages/shiftonelabs/codesniffer-standard
+[link-author]: https://github.com/patrickcarlohickman
